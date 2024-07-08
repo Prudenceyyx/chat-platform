aca13bd99c077104b2dc99bb4afcedab9b67bcdc
Prudencé <yy1308@nyu.edu>
2024-07-08 18:06:04 +0800

    style: Fix nav margin

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-// import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import React from "react";
+import Search from './search'
 
 const Nav = (props) => {
   return (
-    <nav className="w-full h-[70px] bg-[#0C0E13] text-white flex justify-between items-center pl-5">
-      <div className='flex items-center'>
+    <nav className="w-full h-[70px] bg-[#0C0E13] text-white flex justify-between items-center pl-5 pr-5">
+      <div className="flex items-center">
         <img src="/assets/icon.svg" />
-        <div className='font-bold pl-5 text-sm'>Gradual Community</div>
+        <div className="font-bold pl-5 text-sm">Gradual Community</div>
       </div>
-      <div className="flex">
-        <div>Search</div>
-        <div>UTC-Chicago</div>
+      <div className="flex items-center">
+        <Search />
+        <div className="flex ml-[20px] shrink-0">
+          <img className="h-[24px] w-[24px]" src="/assets/icons/globe.svg" />
+          <div className="ml-2 shrink-0">UTC-Chicago</div>
+        </div>
 
-        <div>icon</div>
-        <div>avatar</div>
+        <img
+          className="h-[24px] w-[24px] ml-[20px]"
+          src="/assets/icons/bell.svg"
+        />
+        <img
+          className="h-[24px] w-[24px] ml-[20px]"
+          src="/assets/icons/help.svg"
+        />
+        <div className="h-[40px] w-[40px] ml-[20px] shrink-0 rounded-full bg-white"></div>
       </div>
     </nav>
   );
