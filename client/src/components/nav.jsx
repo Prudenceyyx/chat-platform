ed3ae0960b51cfec7929c264c5ab26185bf160d5
Prudencé <yy1308@nyu.edu>
2024-07-10 03:32:51 +0800

    feat: Add username tag

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Search from './search'
-import Avatar from './avatar';
+import Search from "./search";
+import Avatar from "./avatar";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
 const Nav = (props) => {
-const username = localStorage.getItem('chat_username')
+  const username = localStorage.getItem("chat_username");
 
   return (
     <nav className="w-full h-[70px] bg-[#0C0E13] text-white flex justify-between items-center pl-5 pr-5">
@@ -26,8 +27,20 @@ const username = localStorage.getItem('chat_username')
           className="h-[24px] w-[24px] ml-[20px]"
           src="/assets/icons/help.svg"
         />
-        <Avatar className="shrink-0 ml-[20px]" name={username} />
-
+        <Popover data-open>
+          <PopoverButton className="data-[closed]:border-none">
+            <Avatar className="shrink-0 ml-[20px]" name={username} />
+          </PopoverButton>
+          <PopoverPanel
+            transition
+            anchor="bottom"
+            className="w-fit divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0"
+          >
+            <div className="p-3">
+                <p className="font-semibold text-white text-nowrap">{username}</p>
+            </div>
+          </PopoverPanel>
+        </Popover>
       </div>
     </nav>
   );
