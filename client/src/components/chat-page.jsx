b6fe8c10f55216d3412207b21c125c44d323e10f
Prudenceyyx <yy1308@nyu.edu>
2024-07-23 21:48:04 +0800

    feat: Add channel notification

--- client/src/components/chat-page.jsx
+++ client/src/components/chat-page.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-import Search from "./search";
-import ChatPanel from "./chat-panel";
-import Avatar from "./avatar";
-import { gql, useQuery } from "urql";
+import React, { useEffect, useState } from 'react';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import Search from './search';
+import ChatPanel from './chat-panel';
+import Avatar from './avatar';
+import { gql, useQuery } from 'urql';
+import { socket } from '../App';
 
 const ChannelsQuery = gql`
   query {
@@ -27,19 +28,54 @@ const ChatPage = (props) => {
 
   const { data, fetching, error } = result;
 
+  const [channelData, setChannelData] = useState([]);
+
+  useEffect(() => {
+    if (!fetching) {
+      setChannelData(data.channels);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    let channelIDs = [];
+    if (data) {
+      data.channels.forEach((channel) => {
+        channelIDs.push(channel.id);
+
+        socket.on(`message:${channel.id}`, (msg) => {
+          setChannelData((prev) => {
+            const result = prev.map((prevChannelData) => {
+              if (prevChannelData.id === channel.id) {
+                return { ...prevChannelData, lastMessage: msg };
+              } else {
+                return prevChannelData;
+              }
+            });
+            return result;
+          });
+        });
+      });
+    }
+
+    return () => {
+      if (channelIDs) {
+        channelIDs.forEach((channelID) => {
+          return socket.off(`message:${channelID}`);
+        });
+      }
+    };
+  }, [data]);
+
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
 
   return (
     <TabGroup className="flex w-full h-full grow">
       <TabList className="w-[340px] h-full shrink-0 bg-[#1D1C21]">
-        <div
-          className="h-16 flex items-center"
-          style={{ boxShadow: "0px 1px 0px 0px #FFFFFF1A" }}
-        >
+        <div className="h-16 flex items-center" style={{ boxShadow: '0px 1px 0px 0px #FFFFFF1A' }}>
           <Search />
         </div>
-        {data.channels.map((channel, index) => {
+        {channelData.map((channel, index) => {
           const { lastMessage } = channel;
           return (
             <Tab
@@ -49,12 +85,8 @@ const ChatPage = (props) => {
               <Avatar className="shrink-0" name={channel.name} />
               <div className="w-full px-[10px] ">
                 <div className="flex justify-between w-full items-center">
-                  <div className="text-sm/6 font-medium text-[#C9C7D0]">
-                    {channel.name}
-                  </div>
-                  <div className="text-xs font-medium text-[#7B798F]">
-                    22:04
-                  </div>
+                  <div className="text-sm/6 font-medium text-[#C9C7D0]">{channel.name}</div>
+                  <div className="text-xs font-medium text-[#7B798F]">22:04</div>
                 </div>
                 {lastMessage && (
                   <div className="text-xs font-medium text-[#7B798F] text-left">
@@ -69,10 +101,7 @@ const ChatPage = (props) => {
       <TabPanels className="grow h-full">
         {data.channels.map((channel, index) => {
           return (
-            <TabPanel
-              key={channel.id}
-              className="h-full flex flex-col justify-between bg-[#26252D]"
-            >
+            <TabPanel key={channel.id} className="h-full flex flex-col justify-between bg-[#26252D]">
               <ChatPanel channel={channel} />
             </TabPanel>
           );
